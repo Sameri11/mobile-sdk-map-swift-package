347149f492dcea26a24d75f0949157d93ed246e5
buildserver-v4core <uk.github.auth@2gis.ru>
2021-07-27 11:50:13 +0700

    Bump version to 1.2.1+public

--- Package.swift
+++ Package.swift
@@ -3,17 +3,18 @@
 import PackageDescription
 
 let package = Package(
-    name: "DGisMobileSDK",
-    products: [
-        .library(
-            name: "MapSDK",
-            targets: ["DGis"]),
-    ],
-    targets: [
-		.binaryTarget(
-			name: "DGis",
-			url: "https://artifactory.2gis.dev/sdk-ios-release/1.1.1/Release/DGisFullSDK.zip",
-			checksum: "8e7de3d202bc0d1cc289961654c5918f1cfdb9b625fffa3a38de5edbfcfaae27"
-		)
-    ]
+  name: "DGisMobileSDK",
+  products: [
+    .library(
+      name: "MapSDK",
+      targets: ["DGis"])
+  ],
+  targets: [
+    .binaryTarget(
+      name: "DGis",
+      url:
+        "https://artifactory.2gis.dev/sdk-ios-release/1.2.1+public/Release/DGisMapSDK.zip",
+      checksum: "1ceef7abcb0a138741b8d4cd095008e24b112e6b51b85779e1b473431ef913d1"
+    )
+  ]
 )
